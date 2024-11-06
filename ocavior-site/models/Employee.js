24c74ef6376dc1688ac492ce603cd9353f3015ee
pinaki8911 <58633597+pinaki8911@users.noreply.github.com>
2024-11-07 00:36:42 +0530

    ocavior be

--- ocavior-site/models/Employee.js
+++ ocavior-site/models/Employee.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const employeeSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
-  email: { type: String, unique: true },
+  email: { type: String, unique: false },
   position: String,
   ExpSkill: String,
   resumeUrl: String,
